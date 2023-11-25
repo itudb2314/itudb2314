185bbd89bc4204a3f3509158e7ce685cbf60e98f
danaaljasem <danaaljasem74@gmail.com>
2023-11-26 02:22:23 +0300

    added styling to frontend and necessary join operations

--- backend/db/models/match.py
+++ backend/db/models/match.py
@@ -36,6 +36,12 @@ class Match:
     away_team_win : bool
     draw : bool
 
+    #attributes from join operations
+    stadium_name : str = None
+    city_name : str = None
+    home_team_name : str = None
+    away_team_name : str = None
+
 
 class MatchDAO():
     @staticmethod
@@ -118,7 +124,11 @@ class MatchDAO():
             matches = []
             connection = db.get_connection()
             query = """
-                    SELECT * FROM matches
+                    SELECT m.*, s.stadium_name, s.city_name, thome.team_name, taway.team_name 
+                    FROM matches m 
+                    LEFT JOIN stadiums s ON m.stadium_id = s.stadium_id 
+                    LEFT JOIN teams thome ON m.home_team_id = thome.team_id
+                    LEFT JOIN teams taway ON m.away_team_id = taway.team_id
                     """
             cursor = connection.cursor()
             cursor.execute(query)
@@ -128,7 +138,8 @@ class MatchDAO():
                     match = Match(result[0], result[1], result[2], result[3],result[4], result[5], result[6], 
                             result[7], result[8], result[9], result[10], result[11],result[12], result[13], 
                             result[14], result[15], result[16], result[17], result[18],result[19], result[20], 
-                            result[21], result[22], result[23], result[24], result[25], result[26],result[27])
+                            result[21], result[22], result[23], result[24], result[25], result[26],result[27],
+                            result[28], result[29], result[30], result[31])
                     matches.append(match)
                 return matches
             else:
--- frontend/src/js/Matches.js
+++ frontend/src/js/Matches.js
@@ -5,10 +5,19 @@ export default function Matches() {
     const [matches, setMatches] = useState([]);
 
     useEffect(() => {
-        fetch('http://localhost:5000/matches')
-            .then((response) => response.json())
-            .then((data) => {
-                setMatches(data);
+        fetch('http://localhost:5000/matches') //fetching data from backend
+            .then((response) => response.json()) //converting response to json
+            .then((data) => {  //data is the json response
+                //grouping matches
+                const tournament_matches = data.reduce((tournament, match) => {
+                    const key = match.tournament_id;  //key based on which matches are grouped
+                    if(!tournament[key])   //check if array exits
+                        tournament[key] = [];
+                    tournament[key].push(match);
+                    return tournament;
+                }, {});  //initial value of tournament is an empty object
+                const matches_array = Object.values(tournament_matches); //converting object to array
+                setMatches(matches_array);
             })
             .catch((error) => {
                 console.error('Error fetching data:', error);
@@ -16,33 +25,46 @@ export default function Matches() {
     }, []);
 
     const style = {
-        alignSelf: 'center',
-        padding: '2rem 0 0 2rem',
+        margin: '2rem 0 2rem 0',
+        color: 'reds',
     };
 
     return (
         <div className="matches">
-            <h1 style={style}>Matches</h1>
-            {matches.map((match) => (
-                <Match key={match.match_id} {...match} />
+            <h1>Matches</h1> 
+            {matches.map((tournament_matches, i) => (
+                <div key={i}>
+                    <h2 style={style}>{tournament_matches[0].tournament_id}</h2>
+                    {tournament_matches.map((match) => (
+                        <Match key={match.match_id} {...match} />
+                    ))}
+                </div>
             ))}
         </div>
     );
 }
 
 function Match(match) {
+    const match_style = {
+        border: '1px solid black',
+        margin: '1rem',
+        borderRadius: '50px',
+        width: '75%',
+    }
     return (
-        <div>
-            <h2>
-                {match.match_stage} / {match.match_name}
+        <div style={match_style} className='center_div'>
+            <h2 className = "match_header">
+                {match.tournament_id} / {match.stage_name}
             </h2>
-            <div>
-                <p>match_date: {match.match_date}</p>
-                <p>match_time : {match.match_time}</p>
-                <p>home_team_id: {match.home_team_id}</p>
-                <p>away_team_id: {match.away_team_id}</p>
-                <p>home_team_score: {match.home_team_score}</p>
-                <p>away_team_score: {match.away_team_score}</p>
+            <div className='match_details'>
+                <p className='team_names'>{match.home_team_name}</p>
+                <p className='team_score'>{match.home_team_score}   -   {match.away_team_score}</p>
+                <p className='team_names'>{match.away_team_name}</p>
+            </div>
+            <div className='match_time'>
+                <p className='match_time_item'>{match.match_time}</p>
+                <p className='match_time_item'>{match.stadium_name}</p>
+                <p className='match_time_item'>{match.city_name}</p>
             </div>
         </div>
     );
