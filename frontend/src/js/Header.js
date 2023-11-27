9672a559665817a9f4741a6259ed7cb2bcc9c281
milkygraph <kenanbanda2004@gmail.com>
2023-11-27 14:55:18 +0300

    Update Header.js

--- frontend/src/js/Header.js
+++ frontend/src/js/Header.js
@@ -10,6 +10,7 @@ export default function Header() {
             <li><Link className="menu_item" to="/squads">Squads</Link></li>
             <li><Link className="menu_item" to="/matches">Matches</Link></li>
             <li><Link className="menu_item" to="/teams">Teams</Link></li>
+            <li><Link className="menu_item" to="/groupstandings">Group Standings</Link></li>
         </ul>
     </header>
     );
