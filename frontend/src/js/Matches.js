eea6643b4098f1280ace19ce721cc1edae61537c
danaaljasem <danaaljasem74@gmail.com>
2023-12-06 23:58:20 +0300

    added frontend for delte and insert operations

--- frontend/src/js/Matches.js
+++ frontend/src/js/Matches.js
@@ -8,6 +8,7 @@ export default function Matches() {
     const [match, setMatch] = useState([]);
     const [goals_by_id, setGoals_by_id] = useState([]);
     const {match_id} = useParams();
+    const [matchAdded, setMatchAdded] = useState(false);
 
     useEffect(() => {
         if(!match_id) {
@@ -56,14 +57,147 @@ export default function Matches() {
                 console.error('Error fetching data:', error);
             }); 
         }
-    }, [match_id]);
+    }, [match_id, ]);
     const style = {
         margin: '2rem 0 2rem 0',
         color: 'reds',
     };
 
+    //delete function
+    const deleteMatchbyID = async (match_id) => {
+        await fetch(`http://localhost:5000/matches/${match_id}`, {
+            method: 'DELETE',
+        });
+        setMatches(matches.filter((match) => match.match_id !== match_id));
+    };
+
+    //insert function
+    const [showInsertForm, setShowInsertForm] = useState(false);
+    const toggleInsertForm = () => setShowInsertForm(!showInsertForm);
+    const handleInsertMatch = (event) => {
+        event.preventDefault();
+        const matchdata = {
+            tournament_id: event.target.tournament_id.value,
+            match_id: event.target.match_id.value,
+            match_name: event.target.match_name.value,
+            stage_name: event.target.stage_name.value,
+            group_name: event.target.group_name.value,
+            group_stage: event.target.group_stage.value,
+            knockout_stage: event.target.knockout_stage.value,
+            replayed: event.target.replayed.value,
+            replay: event.target.replay.value,
+            match_date: event.target.match_date.value,
+            match_time: event.target.match_time.value,
+            stadium_id: event.target.stadium_id.value,
+            home_team_id: event.target.home_team_id.value,
+            away_team_id: event.target.away_team_id.value,
+            score: event.target.score.value,
+            home_team_score: event.target.home_team_score.value,
+            away_team_score: event.target.away_team_score.value,
+            home_team_score_margin: event.target.home_team_score_margin.value,
+            away_team_score_margin: event.target.away_team_score_margin.value,
+            extra_time: event.target.extra_time.value,
+            penalty_shootout: event.target.penalty_shootout.value,
+            score_penalties: event.target.score_penalties.value,
+            home_team_score_penalties: event.target.home_team_score_penalties.value,
+            away_team_score_penalties: event.target.away_team_score_penalties.value,
+            result: event.target.result.value,
+            home_team_win: event.target.home_team_win.value,
+            away_team_win: event.target.away_team_win.value,
+            draw: event.target.draw.value
+        };
+
+        fetch('http://localhost:5000/matches', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({matchdata}),
+        })
+        .then((response) => response.json())
+        .then((data) => {
+            toggleInsertForm();
+            setMatchAdded(!matchAdded);
+        })
+        .catch((error) => {
+            console.error('Error:', error);
+        });
+
+        setMatchAdded(!matchAdded);
+    };
+
     return (
         <div className="matches">
+            {/*button to toggle insert form*/}
+            <button onClick={toggleInsertForm}>Insert Match</button>
+            {/*conditional rendering for insertion form*/}
+            {showInsertForm && (
+                <div className='form'>
+                    <div className='form-content'>
+                        <h2>Insert Match</h2> 
+                        <form onSubmit={handleInsertMatch}>
+                            <label> Tournament ID </label>
+                            <input type="text" name="tournament_id" placeholder='WC-YEAR'/>
+                            <label> Match ID </label>
+                            <input type="text" name="match_id" placeholder='M-YEAR-MATCHNUMBER'/>
+                            <label> Match Name </label>
+                            <input type="text" name="match_name" placeholder='HOMETEAM vs AWAYTEAM'/>
+                            <label> Stage Name </label>
+                            <input type="text" name="stage_name" placeholder='ex:Group Stage'/>
+                            <label> Group Name </label>
+                            <input type="text" name="group_name" placeholder='ex:Group 1 or not applicable'/>
+                            <label> Group Stage </label>
+                            <input type="text" name="group_stage" placeholder='True or False'/>
+                            <label> Knockout Stage </label>
+                            <input type="text" name="knockout_stage" placeholder='True or False'/>
+                            <label> Replayed </label>
+                            <input type="text" name="replayed" placeholder='True or False'/>
+                            <label> Replay </label>
+                            <input type="text" name="replay" placeholder='True or False'/>
+                            <label> Match Date </label>
+                            <input type="text" name="match_date" placeholder='YYYY-MM-DD'/>
+                            <label> Match Time </label>
+                            <input type="text" name="match_time" placeholder='HH:MM'/>
+                            <label> Stadium ID </label>
+                            <input type="text" name="stadium_id" placeholder='S-NUMBER'/>
+                            <label> Home Team ID </label>
+                            <input type="text" name="home_team_id" placeholder='T-NUMBER'/>
+                            <label> Away Team ID </label>
+                            <input type="text" name="away_team_id" placeholder='T-NUMBER'/>
+                            <label> Score </label>
+                            <input type="text" name="score" placeholder='HomeTeamScore - AwayTeamScore'/>
+                            <label> Home Team Score </label>
+                            <input type="text" name="home_team_score"/>
+                            <label> Away Team Score </label>
+                            <input type="text" name="away_team_score"/>
+                            <label> Home Team Score Margin </label>
+                            <input type="text" name="home_team_score_margin" placeholder='HomeTeamScoreMargin = HomeTeamScore - AwayTeamScore'/>
+                            <label> Away Team Score Margin </label>
+                            <input type="text" name="away_team_score_margin" placeholder='AwayTeamScoreMargin = AwayTeamScore - HomeTeamScore'/>
+                            <label> Extra Time </label>
+                            <input type="text" name="extra_time" placeholder='True or False'/>
+                            <label> Penalty Shootout </label>
+                            <input type="text" name="penalty_shootout" placeholder='True or False'/>
+                            <label> Score Penalties </label>
+                            <input type="text" name="score_penalties" placeholder='HomeTeam - AwayTeam'/>
+                            <label> Home Team Score Penalties </label>
+                            <input type="text" name="home_team_score_penalties"/>
+                            <label> Away Team Score Penalties </label>
+                            <input type="text" name="away_team_score_penalties"/>
+                            <label> Result </label>
+                            <input type="text" name="result" placeholder='HomeTeamWin or AwayTeamWin or Draw'/>
+                            <label> Home Team Win </label>
+                            <input type="text" name="home_team_win" placeholder='True or False'/>
+                            <label> Away Team Win </label>
+                            <input type="text" name="away_team_win" placeholder='True or False'/>
+                            <label> Draw </label>
+                            <input type="text" name="draw" placeholder='True or False'/>
+                            <button type = "submit">Insert</button> 
+                        </form>
+                        <button onClick={toggleInsertForm}>Close</button>
+                    </div>
+                </div>
+            )}
             {match_id ? (
                 <MatchScoreBoard key={match.match_id}  match={match} goals={goals_by_id}/>
             ) : (
@@ -71,7 +205,7 @@ export default function Matches() {
                     <div key={i}>
                         <h2 style={style}>{tournament_matches[0].tournament_name}</h2>
                         {tournament_matches.map((match) => (
-                            <Match key={match.match_id}  match={match} goals={goals[match.match_id]}/>
+                            <Match key={match.match_id}  match={match} goals={goals[match.match_id]}  onDeleteMatch={deleteMatchbyID}/>
                         ))}
                     </div>
                 )))} 
@@ -79,7 +213,14 @@ export default function Matches() {
     );
 }
 
-function Match({match, goals}) {
+function Match({match, goals, onDeleteMatch}) {
+    const [deleting, setDeleting] = useState(false);
+
+    const handleDeleteMatch = () => {
+        setDeleting(true);
+        onDeleteMatch(match.match_id);
+    }
+
     const match_style = {
         border: '1px solid black',
         margin: '1rem',
@@ -124,6 +265,9 @@ function Match({match, goals}) {
                 ))}
                 </div>
             </div>
+            <button onClick={handleDeleteMatch} disabled={deleting}>
+                {deleting ? 'Deleting...' : 'Delete Match'}
+            </button>
         </div>
     );
 }
@@ -135,26 +279,26 @@ function MatchScoreBoard({match, goals}) {
             <div className="team">
               <h2>{match.home_team_name}</h2>
               <ul className="goals">
-                {goals && goals
+                {Array.isArray(goals) ? (goals
                   .filter((goal) => goal.team_id === match.home_team_id)
                   .map((goal, index) => (
                     <li key={index}>
                       <p>{goal.minute_label} {goal.given_name} {goal.family_name}</p>
                     </li>
-                  ))}
+                  ))) : null}
               </ul>
             </div>
             <div className="vs">{match.home_team_score} - {match.away_team_score}</div>
             <div className="team">
               <h2>{match.away_team_name}</h2>
               <ul className="goals">
-                {goals && goals
+                {Array.isArray(goals) ? (goals
                   .filter((goal) => goal.team_id === match.away_team_id)
                   .map((goal, index) => (
                     <li key={index}>
                       <p>{goal.minute_label} {goal.given_name} {goal.family_name}</p>
                     </li>
-                  ))}
+                  ))) : null}
               </ul>
             </div>
           </div>
